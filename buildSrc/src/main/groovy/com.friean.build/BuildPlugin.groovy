4dbc0e9d978f462834f6beb928bb8b6d298571fa
Fei <954455187@qq.com>
2019-09-10 11:39:35 +0800

    add custom gradle plugin

---
+++ buildSrc/src/main/groovy/com.friean.build/BuildPlugin.groovy
@@ -0,0 +1,12 @@
+package com.friean.build
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class BuildPlugin implements Plugin<Project>{
+
+    @Override
+    void apply(Project project) {
+        println 'hello plugin build'
+    }
+}
