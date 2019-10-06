548ced9fe8309d34a4afdbda110ac1f32c0784a6
Fei <954455187@qq.com>
2019-10-06 16:34:42 +0800

    convert to androidx

--- androidbase/src/main/java/com/friean/androidbase/handler/HandlerActivity.java
+++ androidbase/src/main/java/com/friean/androidbase/handler/HandlerActivity.java
@@ -2,9 +2,8 @@ package com.friean.androidbase.handler;
 
 import android.annotation.SuppressLint;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
